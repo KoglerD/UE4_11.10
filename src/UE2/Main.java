f8adfb84d814b6e277b5d49fd9c498546277f297
KoglerD <koglerd190073@sus.htl-grieskirchen.at>
2021-11-08 10:53:27 +0100

    UE2 werkt nicht

--- src/UE2/Main.java
+++ src/UE2/Main.java
@@ -1,56 +1,71 @@
 package UE2;
 
-import java.util.ArrayList;
-import java.util.ConcurrentModificationException;
-import java.util.List;
-import java.util.Scanner;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.*;
+import java.util.concurrent.*;
 
 public class Main {
+
+    private static int solution;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(8);
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(8);
 
         System.out.print("Zahl> ");
         long zahl = Integer.parseInt(scanner.nextLine());
         int min = 1;
         int max = 100;
-        int ergebnis = 0;
-        List<Integer> subList = new ArrayList<>();
+        long chunk = 0;
+        int[] array = new int[(int)zahl];
+        Future<Integer> result = new CompletableFuture<>();
+        /*Callable<int[]> c = () -> {
+            Arrays.sort(array);
+            return array;
+        };*/
 
-        for (int i = 0; i < zahl--; i++) {
-            if (max <= zahl - max){
-                for (int x = min; x < max; x++) {
+
+            if (zahl % 100 == 0) {
+                chunk = zahl / 100;
+            } else {
+                chunk = zahl / 100 + 1;
+            }
+
+            List<Integer> subList = new ArrayList<>();
+
+        for (int i = 1; i < chunk; i++) {
+            if (max <= (zahl - max)) {
+                for (int x = min; x <= max; x++) {
                     subList.add(x);
                 }
-            }else {
-                for (int x = min; x < zahl; x++) {
+            } else {
+                for (int x = min; x <= zahl; x++) {
                     subList.add(x);
                 }
             }
+            /*result = executor.submit(() -> {
+                return addAllElementsInList(subList);
+            });*/
 
             executor.execute(() -> {
-                addAllElementsInList(subList);
+                solution = addAllElementsInList(subList);
             });
 
             min += 100;
             max += 100;
-            ergebnis += subList.get(i);
         }
 
         executor.shutdown();
-
-        System.out.println(ergebnis);
+        //ergebnis += solution;
+        System.out.println(solution);
     }
 
-    private static void addAllElementsInList(List<Integer> list){
+    private static int addAllElementsInList(List<Integer> list) {
         int addAll = 0;
-        for (int i:list) {
-            addAll +=i;
-            list.remove(i);
+        for (int i : list) {
+            addAll += i;
         }
-        list.add(addAll);
+        list.clear();
+        //list.add(addAll);
+        return addAll;
     }
 }
